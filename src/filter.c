2c59c4a85c8a5c84be21681cc53f4d67f4c8585f
or-or <princesscelestiaisfabulous@gmail.com>
2024-12-04 00:44:50 +0100

    Adds source code for substitution filter and source code sub-directory
    **changes**
    Adds source code for a substitution filter (see  ./src/filter.c).
    
    **purpose**
    Make it harder for ne'er-do-wells to bypassing word filters.
    
    **note**
    Right now the filter is pretty basic, only able to apply a single rule for each character. A neat addition would be adding multiple rules per character.

---
+++ src/filter.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <string.h>
+
+const unsigned char fAddress[] = "words";
+const unsigned char lineLimit = 255;
+
+FILE * fileIn;
+FILE * fileOut;
+
+void substituteCharacter( char * word, int index ) {
+    switch( word[ index ] ) {
+        case 'a':
+            word[ index ] = '@';
+            break;
+        case 'e':
+            word[ index] = '3';
+            break;
+        case 'g':
+            word[ index ] = '6';
+            break;
+        case 'i':
+            word[ index ] = '1';
+            break;
+        case 'o':
+            word[ index ] = '0';
+            break;
+        case 'l':
+            word[ index ] = '1';
+            break;
+        case 'c':
+            word[ index ] = 'k';
+            break;
+        case 'k':
+            word[ index ] = 'c';
+            break;
+    }
+}
+
+void filter( char word[] ) {
+
+    int limit = strlen( word )-1;
+    int index = 0;
+    int set[ limit ];
+    char copy[ limit ];
+    
+    // construct set
+    for( int i = 0; i < limit; i += 1 ) {
+        switch( word[i] ) {
+            case 'a':
+            case 'c':
+            case 'e':
+            case 'g':
+            case 'i':
+            case 'o':
+            case 'k':
+            case 'l':
+                set[index] = i;
+                index += 1;
+                break;
+        }
+    }
+
+    // construct power set
+    // GfG implementation no author credited
+    for( int i = 0; i < ( 1 << index ); i += 1 ) {
+        strcpy( copy, word );
+        for( int j = 0; j < index; j += 1 ) {
+            if( i & ( 1 << j ) ) {
+                substituteCharacter( copy, set[ j ] );
+            }
+        }
+        //printf( "%s", copy );
+        fputs( copy, fileOut );
+    }
+}
+
+
+int main() {
+    char line[ lineLimit ];
+
+    fileIn = fopen( fAddress, "r" );
+    fileOut = fopen( "words_out", "w" );
+
+    if( fileIn == NULL ) {
+        printf( "No valid input file." );
+        fclose( fileIn );
+        return 0;
+    }
+
+    while( fgets( line, lineLimit, fileIn ) ) {
+        filter( line );
+    };
+
+    fclose( fileIn );
+    fclose( fileOut );
+
+    return 0;
+}
